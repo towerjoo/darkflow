1807caf57d22b9fbbf5caf71c5e220f841496389
Zhu Tao <zhutao.iscas@gmail.com>
2017-04-14 20:30:05 +0800

    use blank instead of tab

--- net/yolov2/test.py
+++ net/yolov2/test.py
@@ -9,7 +9,7 @@ from utils.box import BoundBox
 from cython_utils.cy_yolo2_findboxes import box_constructor
 
 def expit(x):
-	return 1. / (1. + np.exp(-x))
+    return 1. / (1. + np.exp(-x))
 
 def _softmax(x):
     e_x = np.exp(x - np.max(x))
@@ -17,64 +17,64 @@ def _softmax(x):
     return out
 
 def findboxes(self, net_out):
-	# meta
-	meta = self.meta
-	boxes = list()
-	boxes=box_constructor(meta,net_out)
-	return boxes
+    # meta
+    meta = self.meta
+    boxes = list()
+    boxes=box_constructor(meta,net_out)
+    return boxes
 
 def postprocess(self, net_out, im, save = True, return_pred=True):
-	"""
-	Takes net output, draw net_out, save to disk
-	"""
-	boxes = self.findboxes(net_out)
+    """
+    Takes net output, draw net_out, save to disk
+    """
+    boxes = self.findboxes(net_out)
 
-	# meta
-	meta = self.meta
-	threshold = meta['thresh']
-	colors = meta['colors']
-	labels = meta['labels']
-	if type(im) is not np.ndarray:
-		imgcv = cv2.imread(im)
-	else: imgcv = im
-	h, w, _ = imgcv.shape
-	
-	textBuff = "["
+    # meta
+    meta = self.meta
+    threshold = meta['thresh']
+    colors = meta['colors']
+    labels = meta['labels']
+    if type(im) is not np.ndarray:
+        imgcv = cv2.imread(im)
+    else: imgcv = im
+    h, w, _ = imgcv.shape
+    
+    textBuff = "["
     pred = []
-	for b in boxes:
-		boxResults = self.process_box(b, h, w, threshold)
-		if boxResults is None:
-			continue
-		left, right, top, bot, mess, max_indx, confidence = boxResults
-		thick = int((h + w) // 300)
+    for b in boxes:
+        boxResults = self.process_box(b, h, w, threshold)
+        if boxResults is None:
+            continue
+        left, right, top, bot, mess, max_indx, confidence = boxResults
+        thick = int((h + w) // 300)
         pred.append({
             "label": mess,
             "confidence": confidence,
         })
-		if self.FLAGS.json:
-			line = 	('{"label":"%s",'
-					'"confidence":%.2f,'
-					'"topleft":{"x":%d,"y":%d},'
-					'"bottomright":{"x":%d,"y":%d}},\n') % \
-					(mess, confidence, left, top, right, bot)
-			textBuff += line
-			continue
+        if self.FLAGS.json:
+            line =     ('{"label":"%s",'
+                    '"confidence":%.2f,'
+                    '"topleft":{"x":%d,"y":%d},'
+                    '"bottomright":{"x":%d,"y":%d}},\n') % \
+                    (mess, confidence, left, top, right, bot)
+            textBuff += line
+            continue
 
-		cv2.rectangle(imgcv,
-			(left, top), (right, bot),
-			colors[max_indx], thick)
-		cv2.putText(imgcv, mess, (left, top - 12),
-			0, 1e-3 * h, colors[max_indx],thick//3)
+        cv2.rectangle(imgcv,
+            (left, top), (right, bot),
+            colors[max_indx], thick)
+        cv2.putText(imgcv, mess, (left, top - 12),
+            0, 1e-3 * h, colors[max_indx],thick//3)
 
-	if not save: return imgcv, pred
-	# Removing trailing comma+newline adding json list terminator.
-	textBuff = textBuff[:-2] + "]"
-	outfolder = os.path.join(self.FLAGS.test, 'out')
-	img_name = os.path.join(outfolder, im.split('/')[-1])
-	if self.FLAGS.json:
-		textFile = os.path.splitext(img_name)[0] + ".json"
-		with open(textFile, 'w') as f:
-			f.write(textBuff)
-		return
+    if not save: return imgcv, pred
+    # Removing trailing comma+newline adding json list terminator.
+    textBuff = textBuff[:-2] + "]"
+    outfolder = os.path.join(self.FLAGS.test, 'out')
+    img_name = os.path.join(outfolder, im.split('/')[-1])
+    if self.FLAGS.json:
+        textFile = os.path.splitext(img_name)[0] + ".json"
+        with open(textFile, 'w') as f:
+            f.write(textBuff)
+        return
 
-	cv2.imwrite(img_name, imgcv)
+    cv2.imwrite(img_name, imgcv)
